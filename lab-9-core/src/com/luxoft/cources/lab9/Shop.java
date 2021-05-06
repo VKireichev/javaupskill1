e6aa8fd325ee953eb5ebb8bd8385844b858ce24a
Denis Tsyganov <dtsyganov@luxoft.com>
2021-05-06 17:55:06 +0600

    Lab 9 - ok
    1. Нужно все помещать в пакеты, иначе трудно запускать, т.к. присутствуют классы с одинаковым именем

--- lab-9-core/src/com/luxoft/cources/lab9/Shop.java
+++ lab-9-core/src/com/luxoft/cources/lab9/Shop.java
@@ -1,5 +1,7 @@
+package com.luxoft.cources.lab9;
 
-import java.util.*;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Shop {
     private Scanner in;
@@ -7,18 +9,19 @@ public class Shop {
 
     public Shop(Scanner in) {
         this.in = in;
-        clients = new TreeMap<String, TreeMap<String, Integer>>();
+        clients = new TreeMap<>();
+    }
+
+    public static void main(String[] args) {
+        var shop = new Shop(new Scanner(System.in));
+        shop.makePurchases();
+        shop.printStatistic();
     }
 
     public void makePurchases() {
-        String inputString = "";
-        String clientName = "";
-        String productName = "";
-        Integer productCount = 0;
-
         while (true) {
             System.out.print("Enter: 'client_name product_name count' or 'q' to quit :> ");            // Print a prompt
-            inputString = in.nextLine();
+            var inputString = in.nextLine();
             if ("q".equals(inputString)) {         // EXIT sign
                 return;
             }
@@ -26,17 +29,15 @@ public class Shop {
             String[] parts = inputString.split(" +");
             if (parts.length != 3) {
                 System.out.println("Wrong number of arguments! Retry!" + parts.length + " " + parts[0]);
-                inputString = "";
                 continue;
             }
             try {
-                clientName = parts[0];
-                productName = parts[1];
-                productCount = Integer.parseInt(parts[2]);
+                var clientName = parts[0];
+                var productName = parts[1];
+                var productCount = Integer.parseInt(parts[2]);
                 updateClientsMap(clientName, productName, productCount);
             } catch (NumberFormatException e) {
                 System.out.println("invalid number. " + e.getMessage() + " Retry!");
-                inputString = "";
             } catch (Exception e) {
                 System.out.println("Enexpected error. " + e.getMessage());
                 return;
@@ -57,26 +58,9 @@ public class Shop {
     }
 
     private void updateClientsMap(String clientName, String productName, int productCount) {
-        TreeMap<String, Integer> client;
-        if (!clients.containsKey(clientName)) {
-            client = new TreeMap<String, Integer>();
-            client.put(productName, productCount);
-            clients.put(clientName, client);
-        }
-        else {
-            client = clients.get(clientName);
-            if (!client.containsKey(productName)) {
-                client.put(productName, productCount);
-            } else {
-                int oldProductCount = client.get(productName);
-                client.put(productName, productCount + oldProductCount);
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        Shop shop = new Shop(new Scanner(System.in));
-        shop.makePurchases();
-        shop.printStatistic();
+        clients.putIfAbsent(clientName, new TreeMap<>());
+        TreeMap<String, Integer> client = clients.get(clientName);
+        client.computeIfPresent(productName, (k, v) -> v + productCount);
+        client.putIfAbsent(productName, productCount);
     }
 }
